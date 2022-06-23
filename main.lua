301b5f30d523f2f85b53d3bf6efd601bcc4861ff
Orangoe <chenyao.yin0524@gmail.com>
2022-06-22 23:22:45 -0500

    moving circle

---
+++ main.lua
@@ -0,0 +1,14 @@
+function love.load()
+    circle = {}
+    circle.x = 0
+    circle.y = 100
+end
+
+function love.update(dt)
+    circle.x = circle.x + dt*100
+end
+
+function love.draw()
+    love.graphics.setColor(1, 0.63, 0)
+    love.graphics.circle("fill", circle.x, circle.y, 50)
+end
